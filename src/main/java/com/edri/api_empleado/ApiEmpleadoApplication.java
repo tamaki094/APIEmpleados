23020d8a74178e7e75cd46d734d51721decdd5c3
tamaki094 <edri.alan.94@gmail.com>
2026-04-28 16:08:05 -0600

    Primer Commit

---
+++ src/main/java/com/edri/api_empleado/ApiEmpleadoApplication.java
@@ -0,0 +1,13 @@
+package com.edri.api_empleado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEmpleadoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiEmpleadoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edri/api_empleado/ApiEmpleadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edri.api_empleado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEmpleadoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
